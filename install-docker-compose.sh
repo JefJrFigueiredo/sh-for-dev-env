143c9a900a70fd625ed54c85119497bfd0cb7b5e
Jefferson Figueiredo Jr <jefjr2504@gmail.com>
2023-10-15 22:28:39 -0300

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -4,12 +4,14 @@ echo
 echo "# Install Docker Compose"
 echo
 
-echo "\n\n# Testing Docker as a non-root user"
+echo "# Testing Docker as a non-root user"
 docker run hello-world
 
-echo "\n\n# Install Docker Compose."
+echo 
+echo "# Install Docker Compose."
 sudo apt-get -y update
 sudo apt-get -y install docker-compose-plugin
 
-echo "\n\n# Testing instalation"
+echo 
+echo "# Testing instalation"
 docker compose version
