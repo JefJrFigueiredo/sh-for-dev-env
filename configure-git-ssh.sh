ef4681b4687a6d1cba78f0d2aa408876f2d3c6d6
Jefferson Figueiredo Jr <jefjr2504@gmail.com>
2024-03-07 21:40:09 +0000

    Update configure-git-ssh.sh

--- configure-git-ssh.sh
+++ configure-git-ssh.sh
@@ -19,30 +19,38 @@ echo "Check the list from the folder above to see if you already have SSH keys. 
 # id_ecdsa.pub
 # id_ed25519.pub"
 
-echo
-echo "Do you need to configure the SSH key? (y/n)"
-read able_continue;
-
-while [ $able_continue == 'y' ];
+email_correct='n';
+while [ $email_correct == 'n' ];
 do
-	email_correct='n';
-	while [ $email_correct != 'y' ];
-	do
-		echo
-		echo "Enter your github e-mail:"
-		read email;
-		git config --global user.email "$email"
-		
-		echo
-		echo "Your e-mail is $email. Is it right? (y/n)"
-		read email_correct;
-	done
+	echo
+	echo "What is your github e-mail?"
+	read email;
+	git config --global user.email "$email"
+	
+	echo
+	echo "Your e-mail is $email. Is it right? (y/n)"
+	read email_correct;
+done
 
+name_correct='n';
+while [ $name_correct == 'n' ];
+do
 	echo
 	echo "What is your first name and last name?"
 	read name;
 	git config --global user.name "$name"
+	
+	echo
+	echo "Your name is $name. Is it right? (y/n)"
+	read name_correct;
+done
 
+echo
+echo "Do you need to configure the SSH key? (y/n)"
+read configure_ssh;
+
+while [ $configure_ssh == 'y' ];
+do
 	echo
 	echo "Creating a new SSH key, using the given e-mail. 
 	(Just press Enter to keep the default file name and choose a password.)"
@@ -63,10 +71,14 @@ do
 	xclip -sel clip < ~/.ssh/id_ed25519.pub
 
 	echo
-	echo "Browse to https://github.com/settings/ssh/new > and paste with Ctrl+V on the field 'Key', choose a title and click on 'Add SSH key'."
+	echo "Browse to"
+ 	echo
+  	echo "> > > https://github.com/settings/ssh/new < < <"
+   	echo 
+    	echo "and paste with Ctrl+V on the field 'Key', choose a title and click on 'Add SSH key'."
 	echo "DON'T USE CTRL+C ON ANYTHING, or you will lose the key in your clipboard and you will have to repeat the process."
 
 	echo
 	echo "Do you need to repeat the process of adding the SSH key on github with the instruction above? (y/n)"
-	read able_continue;
+	read configure_ssh;
 done
