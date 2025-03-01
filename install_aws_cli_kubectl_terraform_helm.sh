ca7bfe7b9aebbeab27854d378cb6de685d24f73c
Jefferson Figueiredo Jr. <jefjr2504@gmail.com>
2025-03-01 20:21:00 +0000

    Update install_aws_cli_kubectl_terraform_helm.sh

--- install_aws_cli_kubectl_terraform_helm.sh
+++ install_aws_cli_kubectl_terraform_helm.sh
@@ -1,6 +1,60 @@
 #!/usr/bin/env sh
 
-echo "Script for installing AWS CLI, kubectl, Terraform, and Helm on Ubuntu"
+echo "Script for installing kubectl, Terraform, Helm and AWS CLI on Ubuntu"
+
+echo
+echo "Install kubectl on Linux"
+echo "Source: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/"
+
+echo
+echo "Install kubectl binary with curl on Linux"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+echo
+echo "Validate the binary (optional)"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+echo
+echo "Install kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo
+echo "Verify kubectl installation"
+kubectl version --client
+
+echo
+echo "Remove the downloaded files"
+rm kubectl kubectl.sha256
+
+echo
+echo "Install Terraform on Ubuntu"
+echo "Source: https://developer.hashicorp.com/terraform/install#linux"
+
+echo 
+echo "Update the package index"
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y terraform
+
+echo
+echo "Verify Terraform installation"
+terraform -version
+
+echo
+echo "Install Helm on Debian/Ubuntu"
+echo "Source: https://helm.sh/docs/intro/install/"
+
+echo
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
+echo
+echo "Verify Helm installation"
+helm version
 
 echo "Install AWS CLI"
 echo "Source: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html"
@@ -15,6 +69,9 @@ sudo apt install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+echo
+echo "Verify AWS CLI installation"
 aws --version
 
 echo "Remove the downloaded files"
@@ -53,55 +110,14 @@ echo "    > Default output format [None]: yaml"
 aws configure
 
 echo
-echo "Install kubectl on Linux"
-echo "Source: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/"
-
-echo
-echo "Install kubectl binary with curl on Linux"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
-echo
-echo "Validate the binary (optional)"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-
-echo
-echo "Install kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-echo
-echo "Verify the installation"
+echo "Verify kubectl installation"
 kubectl version --client
-
 echo
-echo "Remove the downloaded files"
-rm kubectl kubectl.sha256
-
-echo
-echo "Install Terraform on Ubuntu"
-echo "Source: https://developer.hashicorp.com/terraform/install#linux"
-
-echo 
-echo "Update the package index"
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install -y terraform
-
-echo
-echo "Verify the installation"
+echo "Verify Terraform installation"
 terraform -version
-
 echo
-echo "Install Helm on Debian/Ubuntu"
-echo "Source: https://helm.sh/docs/intro/install/"
-
-echo
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install -y helm
-
-echo
-echo "Verify the installation"
+echo "Verify Helm installation"
 helm version
+echo
+echo "Verify AWS CLI installation"
+aws --version
